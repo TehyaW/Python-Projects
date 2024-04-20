4e2438e8eda8d94f78d8a48908b2f10fd811d2d4
Honu <134723662+Honublox@users.noreply.github.com>
2024-04-20 18:30:23 -0400

    Create MadLibsGenerator.py

---
+++ Mad Libs Generator/MadLibsGenerator.py
@@ -0,0 +1,183 @@
+def beachDay():
+    print("Beach Day!!! (12 Inputs)")
+    place = input("Place: ")
+    person = input("Person's Name: ")
+    adj2 = input("Adjective: ")
+    noun = input("Noun: ")
+    noun2 = input("Noun: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    noun3 = input("Noun: ")
+    number = input("Number: ")
+    noun4 = input("Noun: ")
+    food = input("Food: ")
+    verb = input("Verb: ")
+    bdMadLib = f"I went to the beach in {place} today with {person}. We had a {adj2} time. \
+We first built a {noun} and then we ran around in the {noun2} for a little while. \
+The water was a bit {adj3}. The sun was very {adj4} so we made sure to wear a lot of {noun3}. \
+We spent {number} hours there. At the end of the day, we treated ourselves to a {noun4} and had {food} for dinner. \
+I definitely want to go to the beach again but next time I want to {verb}."
+    print(bdMadLib)
+
+
+def movies():
+    print("Movies!!! (15 Inputs)")
+    person = input("Person's Name: ")
+    person2 = input("Person's Name: ")
+    movie = input("Movie: ")
+    adj = input("Adjective: ")
+    verb = input("Verb (-ed): ")
+    noun = input("Noun: ")
+    candy = input("Candy: ")
+    food = input("Food: ")
+    verb2 = input("Verb (-ing): ")
+    verb3 = input("Verb (-ing): ")
+    verb4 = input("Verb (-ed): ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    adj5 = input("Adjective: ")
+    mMadlib = f"I went to the movies yesterday with {person} and {person2}. We saw {movie}. \
+It was {adj}. At one part, I even {verb} and ran for the {noun}. During the movie, we ate {candy} and {food}. \
+I got mad because the person sitting behind me kept {verb2} during the movie and wouldn't stop {verb3} either. \
+He was asked to leave after he {verb4} across the theatre. It was pretty {adj2}. \
+Overall, I liked the movie because it was {adj3} and the main character was super {adj4}. \
+Hopefully next time the people sitting behind me will be more {adj5}."
+    print(mMadlib)
+
+
+def australia():
+    print("Vacation!!! (14 Inputs)")
+    place = input("Place: ")
+    person = input("Person's Name: ")
+    number = input("Number: ")
+    petName = input("Pet Name: ")
+    petType = input("Pet Type: ")
+    food = input("Food: ")
+    adj = input("Adjective: ")
+    verb = input("Verb (-ing): ")
+    work = input("Work: ")
+    noun = input("Noun: ")
+    adj2 = input("Adjective: ")
+    verb2 = input("Verb: ")
+    verb3 = input("Verb: ")
+    adj3 = input("Adjective: ")
+    aMadLib = f"I decided to go on a vacation to {place} with {person}. \
+We got to the airport {number} hours early. When we went through security, I got stopped because I forgot to \
+take {petName}, my pet {petType} out of my pocket. We got some {food} for the flight and arrived at the gate. \
+Once we boarded the plane, I was sitting next to a very {adj} man. He spent the entire flight {verb} \
+and talking about his job doing {work}. Whenever I tried to sleep, he would step around me to go to the {noun}. \
+I was so {adj2}. Since I couldn't sleep, I decided to {verb2} and {verb3} instead. \
+Finally, we arrived in {place}. All in all, the flight was {adj3}!"
+    print(aMadLib)
+
+
+def ghostStory():
+    print("Ghost Story!!! (40 Inputs)")
+    room = input("Room or Building: ")
+    adj = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    smell = input("Smell: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    noun = input("Noun: ")
+    noun2 = input("Noun: ")
+    adj5 = input("Adjective: ")
+    verb = input("Verb: ")
+    verb2 = input("Verb (-ed): ")
+    number = input("Number: ")
+    bodyPart = input("Body Part: ")
+    verb3 = input("Verb (-ed): ")
+    adverb = input("Adverb: ")
+    verb4 = input("Verb: ")
+    adj6 = input("Adjective: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    verb5 = input("Verb: ")
+    verb6 = input("Verb (-ing): ")
+    verb7 = input("Verb: ")
+    verb8 = input("Verb (-ed): ")
+    verb9 = input("Verb (-ed): ")
+    verb10 = input("Verb: ")
+    name = input("Name: ")
+    verb11 = input("Verb (-ing): ")
+    verb12 = input("Verb (-ed): ")
+    verb13 = input("Verb (-ed): ")
+    verb14 = input("Verb (-ed): ")
+    verb15 = input("Verb (-ed): ")
+    bodyPart2 = input("Body Part: ")
+    verb16 = input("Verb (-ing): ")
+    adj7 = input("Adjective: ")
+    verb17 = input("Verb (-ing): ")
+    adverb2 = input("Adverb: ")
+    name2 = input("Name: ")
+    verb18 = input("Verb (-ing): ")
+    noun5 = input("Noun (plural): ")
+    noun6 = input("Noun: ")
+    gsMadlib = f"We weren't allowed to go in the {room} and we really didn't want to, it was {adj} and {adj2} \
+and smelled like {smell}.The steps leading down into the {room} were {adj3} and there was a {adj4} {noun} \
+next to the {noun2}. It felt {adj5} and we wanted to {verb}. We {verb2} toward the {room}, {number} \
+steps at a time till our {bodyPart}s {verb3} {adverb} and we were too scared to {verb4}. \
+There was always a {adj6} {noun3} coming up from below the {noun4} and we weren't brave enough to {verb5} it. \
+One day there was a {verb6} sound. It was coming from the {room}. We wanted to know what was making the sound, \
+but we were afraid we might {verb7}. We {verb8} and {verb9}, but each time the fear would {verb10} over us, \
+and we would back away, terrified. Finally, {name} decided to face the fear and go down to the {room}. \
+When we didn't hear {verb11} we became encouraged and followed behind, praying we would not be {verb12} by \
+whatever it was that {verb13} and {verb14} down there. When we made it to the very bottom,\
+we {verb15}, {bodyPart2}s wrapped tightly around each other, barely {verb16}. \
+Then, we saw it. It was {adj7} and it was {verb17} {adverb2}. We moved closer, we couldn't breathe...\
+It was {name2} {verb18} the {noun5} in the {noun6}."
+    print(gsMadlib)
+
+
+def loveLetter():
+    print("Love Letter!!! (21 Inputs)")
+    name = input("Name: ")
+    time = input("Time: ")
+    adj = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    verb = input("Verb: ")
+    bodyPart2 = input("Body Part: ")
+    adj3 = input("Adjective: ")
+    bodyPart3 = input("Body Part: ")
+    verb2 = input("Verb: ")
+    day = input("Day: ")
+    adj4 = input("Adjective: ")
+    adj5 = input("Adjective: ")
+    adj6 = input("Adjective: ")
+    verb3 = input("Verb: ")
+    verb4 = input("Verb: ")
+    adj7 = input("Adjective: ")
+    clothing = input("Clothing: ")
+    holiday = input("Holiday: ")
+    verb5 = input("Verb: ")
+    noun = input("Noun: ")
+    name2 = input("Your name: ")
+    lMadlib = f"Dear {name}: \
+I woke up this {time} and I blushed at the thought of you. \
+You are not only {adj}, but very {adj2}. Every time I {verb} you \
+my {bodyPart2}s become {adj3} and my {bodyPart3}s begin to {verb2}. \
+I can't wait till {day} night. I have a {adj4} surprise for you. \
+It is {adj5}, {adj6}, and {verb3}s alot. \
+I hope you will {verb4} it forever \
+I will be wearing the {adj7} {clothing} you gave me last {holiday}. \
+I know how much you love to {verb5} me in it. \
+Hugs and Kisses, \
+Your Favorite {noun},{name2}"
+    print(lMadlib)
+
+
+storyPick = int(input("Pick a number 1 - 5: "))
+if storyPick == 1:
+    beachDay()
+elif storyPick == 2:
+    movies()
+elif storyPick == 3:
+    australia()
+elif storyPick == 4:
+    ghostStory()
+elif storyPick == 5:
+    loveLetter()
+else:
+    print("Chose a non-supported number, please try again.")
+    quit()
